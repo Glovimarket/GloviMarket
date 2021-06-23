3b6e913a6849a690987db0de5d3c439890ea9837
IIFelipeII <72109168+IIFelipeII@users.noreply.github.com>
2021-06-23 12:49:33 -0500

    Docs: Se agrega insert-joins

---
+++ Segundo Ttrimestre/ BASE DE DATOS/INSERT-JOINS/INSERT-JOINS-CONSULTAS.sql
@@ -0,0 +1,167 @@
+--INSERT
+
+insert into adm (id_adm, primer_nombre_adm ,segundo_nombre_adm,primer_apellido_adm ,segundo_apellido_adm ,telefono_adm ,direccion_adm,email_adm,contraseña_adm)
+values
+    ("1013576811","Andres","Felipe","Orjuela","","3212151254","Cll 95 Sur #5G 12 Este","aforjuela11@misena.edu.co","Andres2004"),
+    ("1022922706","Valeria","Criollo","Cardenas","","3223377850","Cra 11 # 65-70 sur Int 3","vcriollo6@misena.edu.co","Valeria1009"),
+    ("1013099140","Milton","Stiven","Gonzalez","Pinzon","3042217481","Cra 72 #76 a 45 sur","miltonstivenpinzon@gmail.com","Stiven0202"),
+    ("1023062345","Jhoan","Daniel","Mendez","Castañeda","3208187790","Cl. 136  #101 b15","jdmendez698@misena.edu.co","Daniel2929");
+
+insert into cliente (id_cliente, primer_nombre_cli ,segundo_nombre_cli,primer_apellido_cli ,segundo_apellido_cli ,telefono_cli ,dirccion_cli,email_cli,contraseña_cli)
+values
+    ("1013576811","Andres","Felipe","Orjuela","","3212151254","Cll 95 Sur #5G 12 Este","aforjuela11@misena.edu.co","3212151254Andres"),
+    ("1012316243","andres","felipe","monroy","moreno","3204125846","dg 74 sur #78i39","afmonroy34@misena.edu.co","3204125846andres"),
+    ("1006051207","Camilo","Andres","Osorio","colmenares","3235940505","Cll130#94-08","Camiloandresosoriocolmenares@gmail.com","3235940505Camilo"),
+    ("1001271562","Carlos","Alfredo","López","Garzón","3197729848","Cra 2 este #79-08 Sur","calopez2651@misena.edu.co","3197729848Carlos"),
+    ("1000807149","Daniela","Valentina","FonsecDavid","","3015554404","Cra 47b#71-28 sur","Dvfonseca94@misena.edu.co ","3015554404Daniela"),
+    ("1000350620","Esteban","David","Pedrozo","Aldana","3044568380","Ac 68 sur No 69-45","edpedrozo02@misena.edu.co","3044568380Esteban"),
+    ("1023903053","Gary","Daniela","Vargas","Saenz","3219240401","cr 8 este # 9-28 sur","gdvargas35@misena.edu.co","3219240401Gary"),
+    ("1034656167","Heelen","Lizeth","Cano","Moreno","3153130156","Cll 78 #78-71 sur ","hlcano76@misena.edu.co","3153130156Heelen"),
+    ("1000157790","Ivan","Mauricio","Cuervo","Campos","3138144742","cra 14 # 91a 71 sur","maocuervo13@gmail.com","3138144742Ivan"),
+    ("1019602056","Jenifer","Carolina","Russi","Benavides","3015551357","Calle79 sur #3-05","Jcrussi6@misena.edu.co","3015551357Jenifer"),
+    ("1000215896","Jhoan","Daniel","Mendez","Castañeda","3208187790","Cl. 136  #101 b15 ","jdmendez698@misena.edu.co","3208187790Jhoan"),
+    ("1016943117","José","Manuel","Posada","Restrepo","3003441688","calle 6a #94a 25 ","josemanuelposada14@gmail.com","3003441688José"),
+    ("1016942358","Juan","David","Camargo","Useche","3043385964"," Cll 4A #93 sur 34 ","jdcamargo853@misena.edu.co","3043385964Juan"),
+    ("1000036556","Juan","Pablo","Contreras","Lopez","3132597471","Calle 128 b bis a #91-67","contreraslopezjuanpablo026@gmail.com","3132597471Juan"),
+    ("1003540758","Kevin","","Pulido","Delgado","3108995144","Cra 4#91-75 sur","kpulido857@misena.edu.co","3108995144Kevin"),
+    ("1192831945","Maria","Paula","Patiño","Aparicio","3172727783","calle 130 d bis # 99 - 27 ","mapu302929@gmail.com ","3172727783Maria"),
+    ("1005573490","Mario","andres","Vélez","Bravo","3226262960","Cl. 129a #94c-56 a 94c","Mariiovelez15@gmail.com","3226262960Mario"),
+    ("1013099140","Milton","Stiven","Gonzalez","Pinzon","3042217481","Cra 72 #76 a 45 sur","miltonstivenpinzon@gmail.com","3042217481Milton"),
+    ("1014176160","Paula","Alejandra","Cuéllar","Rodríguez","3137528493","Carrera 18 G# 15 35 ","pacuellar06@misena.edu.co","3137528493Paula"),
+    ("1001096125","Samuel","Esteban","Castaño","Gonzalez","3012514581","Cll 152A 99-60","samicasta11@gmail ","3012514581Samuel"),
+    ("1000464506","Sebastian","Danilo","Correa","Boyaca","3195890562","Carrera 87 #129c 19 Interior 8","sdcorrea60@misena.edu.co","3195890562Sebastian"),
+    ("1001117202","Sharon","Guissell","Quintero","Espinel","3043748447","carrera 121 N 128 B 21 ","espinelsharon@gmail.com","3043748447Sharon"),
+    ("1007105324","Valentina","","Grillo","Martinez","3209854322","Calle 107 Sur #4-59","vgrillo@misena.edu.co ","3209854322Valentina"),
+    ("1022922706","Valeria","","Criollo","Cardenas","3223377850","Cra 11 # 65-70 sur Int 3","vcriollo6@misena.edu.co","3223377850Valeria"),
+    ("1010051342","Yasmin","Lucia","Moreno","Suarez","3118271726"," Cll 104 #5-62 Sur","ylmoreno243@misena.edu.co","3118271726Yasmin");
+
+
+insert into vendedor (id_vendedor,primer_nombre_vend ,segundo_nombre_vend ,primer_apellido_vend ,segundo_apellido_vend ,telefono_vend ,direccion_vend ,email_vend ,contraseña_vend)
+values
+    ("1013576811","Andres","Felipe","Orjuela","","3212151254","Cll 95 Sur #5G 12 Este","aforjuela11@misena.edu.co","Andres2004"),
+    ("1022922706","Valeria","Criollo","Cardenas","","3223377850","Cra 11 # 65-70 sur Int 3","vcriollo6@misena.edu.co","Valeria1009"),
+    ("1013099140","Milton","Stiven","Gonzalez","Pinzon","3042217481","Cra 72 #76 a 45 sur","miltonstivenpinzon@gmail.com","Stiven0202"),
+    ("1023062345","Jhoan","Daniel","Mendez","Castañeda","3208187790","Cl. 136  #101 b15","jdmendez698@misena.edu.co","Daniel2929");
+
+insert into categoria_producto (id_categoria, nombre_categoria)
+values
+    ("1","Gafas masculinas"),
+    ("2","Gafas femeninas"),
+    ("3","Estuches"),
+    ("4","Gafas de sol masculinas"),
+    ("5","Gafas de sol Femeninas"),
+    ("6","Lentes Formulados"),
+    ("7","marcos unisex");
+
+insert into producto (id_producto ,nombre_producto ,id_categoria_fk,existencia ,valor_unitario ,descripcion )
+values
+    ("101","Lentes Formulados","7","10","73000","Lentes Formulados"),
+    ("102","Marco personalizado","7","20","25000","Marco personalizado"),
+    ("103","Estuche basico","3","22","15000","Estuche basico"),
+    ("1004","Gafas de sol ","7","1","200000","Gafas de sol "),
+    ("1005","Loupin Men","1","12","159500","Gafas Oftálmicas Negro"),
+    ("1006","Ralph Sol Dama","5","12","300000","Ralph Sol Dama"),
+    ("1007","Mira flex Playa","2","20","250000","Mira flex Playa"),
+    ("1008","Sol moniak","2","13","550000","Sol moniak"),
+    ("1009","Police azul","1","12","80000","Police azul"),
+    ("1010","Vogue Dama redonda","2","19","110000","Vogue Dama redonda"),
+    ("1012","Ray-Ban","1","18","300000","Ray-Ban"),
+    ("1013","Jack Sol hombre","4","52","90000","Jack Sol hombre"),
+    ("1014","Paulo Z","1","1","345000","Paulo Z"),
+    ("1015","Luigui carbonato","1","1","449000","Luigui carbonato");
+
+insert into factura (id_cliente_fk,id_producto_fk,id_vendedor_fk,fecha,cantidad,valor_unitario_fk,valor_total)
+values
+    ("1023903053","1005","1013576811","2021-06-7","10","159500","1595000"),
+    ("1034656167","1006","1022922706","2021-06-8","12","300000","3600000"),
+    ("1000157790","1007","1013099140","2021-06-9","1","250000","250000"),
+    ("1019602056","1008","1023062345","2021-06-10","2","550000","1100000"),
+    ("1000215896","1009","1013576811","2021-06-11","3","80000","240000"),
+    ("1016943117","1010","1023062345","2021-06-12","4","110000","440000"),
+    ("1016942358","1012","1023062345","2021-06-13","2","300000","600000"),
+    ("1000036556","1013","1023062345","2021-06-14","5","90000","450000"),
+    ("1003540758","103","1023062346","2021-06-15","6","15000","90000");
+
+
+-- CONSULTAS
+
+    -- Consulta producto por id
+        select * from producto where id_producto =1012;
+        select * from producto where id_producto like '101%' order by existencia DESC;
+
+    -- Consultar todos los producto que esten la la categoria 7
+        select * from producto where id_categoria_fk like '7%';
+        select * from producto where id_categoria_fk = 7;
+
+    -- Consultar todos los producto que en su nombre tengan HOMBRE o DAMA
+        select * from producto where nombre_producto like '%Hombre%' or nombre_producto like '%Dama%';
+
+    -- Consultar todos los produtos que en su nombre tengan SOL y pertenezcan a la cateogoria 4 o 5
+        select * from producto where nombre_producto like '%SOL%' and id_categoria_fk like '4%' or id_categoria_fk like '5%';
+
+    -- Consulta telefono cliente
+        select * from cliente where telefono_cli =3118271726;
+
+    -- Consulta telefono cliente segun correo
+        select primer_nombre_cli, primer_apellido_cli, email_cli, telefono_cli from cliente where email_cli ='edpedrozo02@misena.edu.co';
+
+    -- Consultar factura por numero ID
+        select * from factura where id_factura =22;
+        select * from factura where id_factura like '2%';
+
+    -- Consultar todas las ventas de un vendedor en especifico
+        -- select * from factura where id_vendedor_fk = 1023062345;
+
+
+-- JOINS
+    -- Todos los producto+El nombre de categoria
+         select id_producto ,nombre_producto ,id_categoria_fk, nombre_categoria,existencia , valor_unitario ,descripcion
+         from producto
+         JOIN categoria_producto ON id_categoria = id_categoria_fk;
+
+      --Ventas mayor a menor+ nombre vendedor
+        select id_factura, id_cliente_fk,id_producto_fk,id_vendedor_fk,fecha,cantidad,valor_unitario_fk,valor_total
+        from factura
+        JOIN vendedor ON id_vendedor = id_vendedor_fk
+        order by valor_total DESC;
+
+    -- Factura+datos del cliente
+        select id_factura,id_cliente_fk,primer_nombre_cli ,segundo_nombre_cli,primer_apellido_cli ,segundo_apellido_cli ,telefono_cli ,dirccion_cli,email_cli ,   id_producto_fk,id_vendedor_fk,fecha,cantidad,valor_unitario_fk,valor_total
+        from factura
+        JOIN cliente ON id_cliente = id_cliente_fk;
+            -- Factura+datos del cliente+ where email
+            select id_factura,id_cliente_fk,primer_nombre_cli ,segundo_nombre_cli,primer_apellido_cli ,segundo_apellido_cli ,telefono_cli ,dirccion_cli,email_cli ,   id_producto_fk,id_vendedor_fk,fecha,cantidad,valor_unitario_fk,valor_total
+            from factura
+            JOIN cliente ON id_cliente = id_cliente_fk
+            where email_cli ='Jcrussi6@misena.edu.co';
+
+     -- Factura+datos del cliente+ datos vendedor
+        select id_factura,id_cliente_fk,primer_nombre_cli ,primer_apellido_cli ,telefono_cli ,dirccion_cli,email_cli ,   id_producto_fk,id_vendedor_fk,primer_nombre_vend ,primer_apellido_vend ,telefono_vend ,email_vend,fecha,cantidad,valor_unitario_fk,valor_total
+        from factura
+        JOIN cliente ON id_cliente = id_cliente_fk
+        JOIN vendedor ON id_vendedor = id_vendedor_fk;
+            -- Factura+datos del cliente+ datos vendedor+ where+like
+            select id_factura,id_cliente_fk,primer_nombre_cli ,primer_apellido_cli ,telefono_cli ,dirccion_cli,email_cli ,   id_producto_fk,nombre_producto, id_vendedor_fk,primer_nombre_vend ,primer_apellido_vend ,telefono_vend ,email_vend,fecha,cantidad,valor_unitario_fk,valor_total
+            from factura
+            JOIN producto ON id_producto = id_producto_fk
+            JOIN cliente ON id_cliente = id_cliente_fk
+            JOIN vendedor ON id_vendedor = id_vendedor_fk
+            where primer_nombre_cli like 'J%' and primer_apellido_cli like 'C%';
+
+    -- Factura+nombre del producto= Descripcion del producto=Nombre del cliente+ nombre vendedor
+        select id_factura, id_cliente_fk,primer_nombre_cli, id_producto_fk,nombre_producto, descripcion , id_vendedor_fk,primer_nombre_vend, fecha,cantidad,valor_unitario_fk,valor_total
+        from factura
+        JOIN producto ON id_producto = id_producto_fk
+        JOIN cliente ON id_cliente = id_cliente_fk
+        JOIN vendedor ON id_vendedor = id_vendedor_fk
+        order by id_factura ASC;
+            -- Factura+nombre del producto= Descripcion del producto=Nombre del cliente+ nombre vendedor+ Busquqeda factura especifica
+            select id_factura,id_cliente_fk,primer_nombre_cli, id_producto_fk,nombre_producto, descripcion , id_vendedor_fk,primer_nombre_vend, fecha,cantidad,valor_unitario_fk,valor_total
+            from factura
+            JOIN producto ON id_producto = id_producto_fk
+            JOIN cliente ON id_cliente = id_cliente_fk
+            JOIN vendedor ON id_vendedor = id_vendedor_fk
+            where id_factura like '2%';
+
+
+
